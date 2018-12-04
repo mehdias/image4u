db5030df29b61c3592f86a934ce610d8b16ce22b
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-12-03 20:21:02 -0800

    successfull tests for the edit and update actions

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,18 +2,20 @@ class ImagesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
  
 
-
   def new
     @image = Image.new
   end
 
   def show
     @image = Image.find_by_id(params[:id])
-    if @image.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @image.blank?
   end
   
+  def edit
+    @image = Image.find_by_id(params[:id])
+    return render_not_found if @image.blank?
+  end
+
   def index
   end
 
@@ -25,11 +27,25 @@ class ImagesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def update
+    @image = Image.find_by_id(params[:id])
+      return render_not_found if @image.blank?
+    @image.update_attributes(image_params)
+      if @image.valid?    
+        redirect_to root_path
+      else
+        return render :edit, status: :unprocessable_entity
+      end
+  end
 
   private
 
   def image_params
     params.require(:image).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 
 end
--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -3,13 +3,39 @@ require 'rails_helper'
 RSpec.describe ImagesController, type: :controller do
   describe "images#edit action" do
     it "should successfully show the edit form if the image is found" do
-    
+      image = FactoryBot.create(:image)
+      get :edit, params: {id: image.id}
+      expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 error message if the gram is not found" do
-    
+      get :edit, params: {id: 'NO'}
+      expect(response).to have_http_status(:not_found)
     end
-    
+  end
+
+  describe "images#update action" do
+    it "should allow users to successfully update images" do
+      image = FactoryBot.create(:image, message: "Initial Value")
+      patch :update, params: {id: image.id, image: {message: 'Changed'}}
+      expect(response).to redirect_to root_path
+      image.reload
+      expect(image.message).to eq "Changed"
+    end
+    it "should have http 404 error if the image cannot be found" do
+      patch :update, params: {id: 'NO', image: {message: 'Changed'}}
+      expect(response).to have_http_status(:not_found)
+    end
+    it "should render the edit form with an http status of unprocessable_entity" do
+      image = FactoryBot.create(:image, message: "Initial Value")
+      patch :update, params: {id: image.id, image: {message: ''}}
+      expect(response).to have_http_status(:unprocessable_entity)
+      image.reload
+      expect(image.message).to eq "Initial Value"
+    end
+   
+  end
+
   describe "images#show action" do
     it "should successfully show the page if the image is found" do
       image = FactoryBot.create(:image)
@@ -71,8 +97,6 @@ RSpec.describe ImagesController, type: :controller do
       expect(response).to have_http_status(:unprocessable_entity)
       expect(image_count).to eq Image.count
     end
-
-
 
   end
 end
