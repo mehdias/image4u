3845240ab51b49abcff93cd0b12390bed95fb847
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-12-25 18:23:45 -0800

    final touches and links working

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ApplicationRecord
   validates :message, presence: true
   validates :picture, presence: true
   belongs_to :user
+  has_many :comments
   mount_uploader :picture, PictureUploader
 end
 
